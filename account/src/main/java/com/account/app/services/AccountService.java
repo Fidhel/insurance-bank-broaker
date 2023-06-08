cdee38ca636eb3c71fc3b0b9ba816325fe62b689
fidhel <fidhelcosta@gmail.com>
2023-06-07 21:57:57 -0300

    basic project structure.

---
+++ account/src/main/java/com/account/app/services/AccountService.java
@@ -0,0 +1,28 @@
+package com.account.app.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+
+    public String viewAccount(int id) {
+        return "Essa é minha conta"+id;
+
+    }
+
+    public boolean createAccount(String accountName) {
+        System.out.println("Creating account: " + accountName);
+        return false;
+
+    }
+    public boolean updateAccount(String texto) {
+        System.out.println("Updating account: " + texto);
+        return false;
+    }
+
+    public boolean deleteAccount(String texto) {
+        System.out.println("Deleting account: " + texto);
+        return false;
+    }
+}
